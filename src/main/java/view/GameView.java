3c351b7bfef50183a676f47419a93c18921e6724
Jolly5 <markus.budeus@web.de>
2017-06-27 11:47:53 +0200

    Started implementation of the GameModel

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -4,6 +4,7 @@ import controller.GameController;
 import observe.GameObserver;
 
 public class GameView implements GameObserver {
+	
     private final GameController controller;
 
     public GameView(GameController controller) {
@@ -12,7 +13,7 @@ public class GameView implements GameObserver {
 
     @Override
     public void onError(String message) {
-
+    	
     }
 
     @Override

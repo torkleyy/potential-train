4793ad706bb68ec506f096d807b3dc56faeb8eb9
Jolly5 <markus.budeus@web.de>
2017-06-30 12:11:30 +0200

    Question table in the database was updated.

--- src/main/java/database/DatabaseEditor.java
+++ src/main/java/database/DatabaseEditor.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 
 import question.Question;
 
-public final class DatabaseEditor {
+    public final class DatabaseEditor {
 
     private static DatabaseConnector con = DatabaseConnector.getInstance();
-
-    public static void main(String[] args) {
-        /*addQuestion(
+	
+	public static void main(String[] args) {
+		/*addQuestion(
 				"Herr Kroegers lieblings Fortbewegungsmittel ist, dass...",
 				"Fahrrad",
 				"Auto",
 				"Skateboard");*/
     }
 
-    public static void addQuestion(String question, String a1, String a2, String a3) {
-        ArrayList<String> list = new ArrayList<>(3);
-        list.add(a1);
-        list.add(a2);
-        list.add(a3);
-        con.addQuestion(new Question(question, list));
-    }
 
+	
+	public static void addQuestion(String question, String a1, String a2, String a3) {
+		ArrayList<String> list = new ArrayList<>(3);
+		list.add(a1);
+		list.add(a2);
+		list.add(a3);
+		con.addQuestion(new Question(question, list));
+	}
 }

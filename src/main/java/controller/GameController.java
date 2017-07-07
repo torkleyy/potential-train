0b6f794814b1bf01377535a1b29a7f60fd9c68ae
Jolly5 <markus.budeus@web.de>
2017-07-07 12:33:45 +0200

    Added the GameController and updated the GameView

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import model.GameModel;
+import observe.GameObserver;
+import question.Question;
+
 public class GameController {
+    
+    private GameModel model;
+    
+    public GameController() {
+        model = new GameModel();
+    }
+    
+    public Question getNextQuestion() {
+        return model.getNextQuestion();
+    }
+    
+    public boolean registerAnswer (int id) {
+        return model.registerAnswer(id);
+    }
+    
+    public void addObserver(GameObserver observer) {
+        model.addObserver(observer);
+    }
 }
--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -2,6 +2,7 @@ package view;
 
 import controller.GameController;
 import observe.GameObserver;
+import question.Answer;
 import question.Question;
 import java.awt.Button;
 import java.awt.Color;
@@ -22,7 +23,11 @@ public class GameView extends JFrame implements GameObserver {
      * 
      */
     private static final long serialVersionUID = 1L;
-
+    
+    private Label lblQuestion;
+    private Button answer1;
+    private Button answer2;
+    private Button answer3;
     /**
      * Launch the application.
      */
@@ -65,32 +70,47 @@ public class GameView extends JFrame implements GameObserver {
     public GameView() {
         controller = new GameController();
 
+        Question q = controller.getNextQuestion();
+        System.out.println("Frage: "+ q.toString());
+
         setBounds(100, 100, 450, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Image image = Toolkit.getDefaultToolkit().createImage("H:"+File.separator+"LehrerTriviaQuiz"+File.separator+"logo.jpg");
         JLabel btBild= new JLabel(new ImageIcon(image));
         btBild.setBounds(70,5,460,100);
         btBild.setText("JLabel");
-        add(btBild);
-        Label l=new Label("Frage");  
-        l.setBounds(260,100,80,30);// setting button position 
-        l.setBackground(Color.YELLOW);
-        add(l);//adding button into frame  
-        Button b=new Button("Antwort 1");  
-        b.setBounds(200,150,200,50);// setting button position  
-        add(b);//adding button into frame  
-        Button c=new Button("Antwort 2");  
-        c.setBounds(200,210,200,50);// setting button position  
-        add(c);//adding button into frame  
-        Button d=new Button("Antwort 3");  
-        d.setBounds(200,270,200,50);// setting button position  
-        add(d);//adding button into frame  
-        Button z=new Button("Zur�ck");  
+        getContentPane().add(btBild);
+        lblQuestion=new Label("Frage");  
+        lblQuestion.setBounds(260,100,80,30);// setting button position 
+        lblQuestion.setBackground(Color.YELLOW);
+        getContentPane().add(lblQuestion);//adding button into frame  
+        answer1 =new Button("Antwort 1");  
+        answer1.setBounds(200,150,200,50);// setting button position  
+        getContentPane().add(answer1);//adding button into frame  
+        answer2=new Button("Antwort 2");  
+        answer2.setBounds(200,210,200,50);// setting button position  
+        getContentPane().add(answer2);//adding button into frame  
+        answer3=new Button("Antwort 3");  
+        answer3.setBounds(200,270,200,50);// setting button position  
+        getContentPane().add(answer3);//adding button into frame  
+        Button z=new Button("Zur�ck");
         z.setBounds(10,320,80,25);// setting button position  
-        add(z);//adding button into frame  
-        setSize(600,400);//frame size 300 width and 300 height  
+        getContentPane().add(z);//adding button into frame  
+        setSize(600,400);//frame size 300 width and 300 height
         setLayout(null);//no layout manager  
         setVisible(true);//now frame will be visible, by default not visible
+        getContentPane().setLayout(null);//no layout manager  
+        setVisible(true);//now frame will be visible, by default not visible
+        
+        loadQuestion(q);
+    }
+    
+    private void loadQuestion(Question q) {
+        lblQuestion.setText(q.getQuestion());
+        Answer[] answers = q.getAnswers();
+        answer1.setLabel(answers[0].toString());
+        answer1.setLabel(answers[1].toString());
+        answer1.setLabel(answers[2].toString());
     }
 
 }
